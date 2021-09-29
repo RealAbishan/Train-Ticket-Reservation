0ac99f253983e35fc94885a8ffa9982d0ef5cd3f
Abishan Parameswaran <highoncode09@gmail.com>
2021-09-29 10:09:42 +0530

    Update

--- src/main/java/com/example/trainticketreservation/Repository/SignUpRepository.java
+++ src/main/java/com/example/trainticketreservation/Repository/SignUpRepository.java
@@ -1,7 +1,8 @@
 package com.example.trainticketreservation.Repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SignUpRepository {
+public interface SignUpRepository extends CrudRepository {
 }
--- src/main/java/com/example/trainticketreservation/Service/Impl/SignUpServiceImpl.java
+++ src/main/java/com/example/trainticketreservation/Service/Impl/SignUpServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.trainticketreservation.Service.Impl;
 
 import com.example.trainticketreservation.Service.SignUpService;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SignUpServiceImpl implements SignUpService {
+
+    private static MongoTemplate datastore;
+
 }
